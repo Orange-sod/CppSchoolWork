36457795d50093489b3ddd1d6b8bdd2ead29e284
Christoph Du <kkchristoph@outlook.com>
2025-03-19 23:58:06 -0400

    Commit FROM PC 3.19 midnight with main.cpp finished (already done, need to test on unix)

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -1,6 +1,9 @@
+#include <cstring>
 #include <iostream>
-#include <fstream>
-#include <string>
+#include "HandleInput.h"
+#include "ProcessObject.h"
+#include "OtherInfo.h"
+#include "HTMLGenerator.h"
 
 #include <cgicc/CgiDefs.h>
 #include <cgicc/Cgicc.h>
@@ -19,10 +22,55 @@ int main() {
     cout << "</head>\n";
     cout << "<body>\n";
 
-    string firstObj = cgi(firstObject);
-    string secondObj = cgi(secondObject);
+    try {
+        string firstObj = cgi(firstObject);
+        string secondObj = cgi(secondObject);
 
-    fstream outputFile;
+        int height1 = 0;
+        int width1 = 0;
+        int objNum1 = 0;
+
+        int height2 = 0;
+        int width2 = 0;
+        int objNum2 = 0;
+
+
+        vector<vector<char>> testMatrix1 = handleInput(firstObj,height1,width1);
+        char** arr1 = convertToCharArray(testMatrix1);
+        string msg1;
+        msg1 =  ProcessObject(height1,width1,arr1, objNum1);
+
+        vector<vector<char>> testMatrix2 = handleInput(secondObj,height2,width2);
+        char** arr2 = convertToCharArray(testMatrix2);
+        string msg2;
+        msg2 =  ProcessObject(height2,width2,arr2, objNum2);
+
+        string Addr = "/home/student/xdu/public_html/output/output.html";
+        GenerateHeaders(Addr);
+        string Time = getCurrentDateTime();
+        string ip = getUserIP();
+        string agent = getUserAgent();
+        GenerateSystemInfo(Addr,Time,ip,agent);
+        GenerateOutput(Addr, objNum1 , objNum2, msg1, msg2);
+        GenerateFooters(Addr, objNum1, objNum2);
+
+        if (arr1) {
+            for (int i = 0; i < height1; ++i) {
+                delete[] arr1[i];
+            }
+            delete[] arr1;
+        }
+
+        if (arr2) {
+            for (int i = 0; i < height2; ++i) {
+                delete[] arr2[i];
+            }
+            delete[] arr2;
+        }
+
+    }catch (runtime_error &e) {
+        cerr << e.what() << endl;
+    }
 
     cout << "You can see the output file and message at";
     cout << "<a href = /home/student/xdu/public_html/output/output.html> here </a>\n";
