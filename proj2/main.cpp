741fa88a8ac0fd2aaf54b22732d29a750b2da746
Christoph Du <kkchristoph@outlook.com>
2025-03-20 00:06:21 -0400

    Modified some statement in main.cpp for testing

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -13,16 +13,16 @@ using namespace std;
 using namespace cgicc;
 
 int main() {
-    string outputUrl = "/home/student/xdu/public_html/output/output.html";
-    Cgicc cgi;
-    cout << "Content-type:text/html\r\n\r\n";
-    cout << "<html>\n";
-    cout << "<head>\n";
-    cout << "<title>CPS 3525 Return Web For Project 2</title>\n";
-    cout << "</head>\n";
-    cout << "<body>\n";
-
     try {
+        string outputUrl = "/home/student/xdu/public_html/output/output.html";
+        Cgicc cgi;
+        cout << "Content-type:text/html\r\n\r\n";
+        cout << "<html>\n";
+        cout << "<head>\n";
+        cout << "<title>CPS 3525 Return Web For Project 2</title>\n";
+        cout << "</head>\n";
+        cout << "<body>\n";
+
         string firstObj = cgi(firstObject);
         string secondObj = cgi(secondObject);
 
@@ -68,13 +68,19 @@ int main() {
             delete[] arr2;
         }
 
+        cout << "Process the two textareas and saved successfully\n";
+
+        system("chmod 777 /home/student/xdu/public_html/output/output.html");
+
+        cout << "File permissions changed successfully for" + outputUrl + "\n";
+
+        cout << "You can see the output file and message at";
+        cout << "<a href = /home/student/xdu/public_html/output/output.html> here </a>\n";
+        cout << "</body>\n";
+
     }catch (runtime_error &e) {
         cerr << e.what() << endl;
     }
-
-    cout << "You can see the output file and message at";
-    cout << "<a href = /home/student/xdu/public_html/output/output.html> here </a>\n";
-    cout << "</body>\n";
 }
 
 
