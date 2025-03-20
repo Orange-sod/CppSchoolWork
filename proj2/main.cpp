942e1493b3c5772fb4c2b95f9d4215483ed00ddd
Christoph Du <kkchristoph@outlook.com>
2025-03-20 00:13:02 -0400

    Modified some statement in main.cpp for testing

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -13,43 +13,42 @@ using namespace std;
 using namespace cgicc;
 
 int main() {
+    string Addr = "/home/student/xdu/public_html/output/output.html";
+    Cgicc cgi;
+    cout << "Content-type:text/html\r\n\r\n";
+    cout << "<html>\n";
+    cout << "<head>\n";
+    cout << "<title>CPS 3525 Return Web For Project 2</title>\n";
+    cout << "</head>\n";
+    cout << "<body>\n";
+
+    string firstObj = cgi(firstObject);
+    string secondObj = cgi(secondObject);
+
+    int height1 = 0;
+    int width1 = 0;
+    int objNum1 = 0;
+    string msg1;
+
+    int height2 = 0;
+    int width2 = 0;
+    int objNum2 = 0;
+    string msg2;
+
+    string Time = getCurrentDateTime();
+    string ip = getUserIP();
+    string agent = getUserAgent();
+
     try {
-        string outputUrl = "/home/student/xdu/public_html/output/output.html";
-        Cgicc cgi;
-        cout << "Content-type:text/html\r\n\r\n";
-        cout << "<html>\n";
-        cout << "<head>\n";
-        cout << "<title>CPS 3525 Return Web For Project 2</title>\n";
-        cout << "</head>\n";
-        cout << "<body>\n";
-
-        string firstObj = cgi(firstObject);
-        string secondObj = cgi(secondObject);
-
-        int height1 = 0;
-        int width1 = 0;
-        int objNum1 = 0;
-
-        int height2 = 0;
-        int width2 = 0;
-        int objNum2 = 0;
-
-
         vector<vector<char>> testMatrix1 = handleInput(firstObj,height1,width1);
         char** arr1 = convertToCharArray(testMatrix1);
-        string msg1;
         msg1 =  ProcessObject(height1,width1,arr1, objNum1);
 
         vector<vector<char>> testMatrix2 = handleInput(secondObj,height2,width2);
         char** arr2 = convertToCharArray(testMatrix2);
-        string msg2;
         msg2 =  ProcessObject(height2,width2,arr2, objNum2);
 
-        string Addr = "/home/student/xdu/public_html/output/output.html";
         GenerateHeaders(Addr);
-        string Time = getCurrentDateTime();
-        string ip = getUserIP();
-        string agent = getUserAgent();
         GenerateSystemInfo(Addr,Time,ip,agent);
         GenerateOutput(Addr, objNum1 , objNum2, msg1, msg2);
         GenerateFooters(Addr, objNum1, objNum2);
@@ -68,19 +67,30 @@ int main() {
             delete[] arr2;
         }
 
-        cout << "Process the two textareas and saved successfully\n";
-
-        system("chmod 777 /home/student/xdu/public_html/output/output.html");
-
-        cout << "File permissions changed successfully for" + outputUrl + "\n";
-
-        cout << "You can see the output file and message at";
-        cout << "<a href = /home/student/xdu/public_html/output/output.html> here </a>\n";
-        cout << "</body>\n";
-
     }catch (runtime_error &e) {
+        cerr << "Runtime error: " << e.what() << endl;
         cerr << e.what() << endl;
+    }catch (const exception &e) {
+        cerr << "General error: " << e.what() << endl;
+        cout << e.what() << "</p>";
     }
+
+    cout << "Process the two textareas and saved successfully\n";
+
+    try {
+        int chmodResult = system(("chmod 777 " + Addr).c_str());
+        if (chmodResult != 0) {
+            throw runtime_error("Failed to change file permissions.");
+        }
+        cout << "<p>File permissions changed successfully for " + Addr + "</p>\n";
+    } catch (const exception &e) {
+        cerr << "Chmod failed: " << e.what() << endl;
+        cout << "<p>Error changing file permissions: " << e.what() << "</p>\n";
+    }
+
+    cout << "You can see the output file and message at";
+    cout << "<a href = /home/student/xdu/public_html/output/output.html> here </a>\n";
+    cout << "</body>\n";
 }
 
 
